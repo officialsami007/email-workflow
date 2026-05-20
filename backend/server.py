e7efcae2e664c4f43c4a7066207ed753865dec05
Sami <officialsami007@gmail.com>
2026-05-20 16:48:21 +0600

    deployement env variable

--- backend/server.py
+++ backend/server.py
@@ -29,7 +29,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://localhost:3000",
+        "https://email-workflow-ten.vercel.app",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
